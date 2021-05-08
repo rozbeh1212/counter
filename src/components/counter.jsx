a6cbae32ba4a31205acedd229154a3e0960f25b1
ASUS <eb.mehrdad@gmail.com>
2021-05-08 17:15:13 +0430

    second commit

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,14 +11,37 @@ styles ={
    fontWight:'bold'  
 };
 
+
+handleIncrement(){
+
+
+
+}
+
   render() {
+
+
+
     return (
       <React.Fragment>
-        <image src={this.state.imageUrl} alt="" />
-        <span style={this.styles} className="badge badge-primary m-2">{this.state.count}</span>
-        <button className="btn btn-secondary btn-sm">'hello2'</button>
+        <span style={this.styles} className={this.getBadgeClasses()}>{this.state.count}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">'hello2'</button>
       </React.Fragment>
     );
+  }
+
+formatCount(){
+
+const {count} = this.state;
+return count === 0 ? "zero" : count;
+
+
+}
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
   }
 }
 
